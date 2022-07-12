088c1c032c137a3a794d45d63b3ee8e159060bd1
Louis Carvalho <louis.carvalho98@gmail.com>
2022-07-12 11:21:18 +0200

    Mise en place des reponses

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Question;
+use App\Form\CommentType;
 use App\Form\QuestionType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -39,9 +41,28 @@ class QuestionController extends AbstractController
     }
 
     #[Route('/question/{id}', name: 'question_show')]
-    public function show(Request $request, Question $question): Response
+    public function show(Request $request, Question $question, EntityManagerInterface $em): Response
     {
+        $comment = new Comment();
+        $commentForm = $this->createForm(CommentType::class, $comment);
+        $commentForm->handleRequest($request);
 
-        return $this->render('question/show.html.twig', ['question' => $question]);
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+            $comment->setCreatedAt(new \DateTimeImmutable())
+                ->setRating(0)
+                ->setQuestion($question);
+
+            $em->persist($comment);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre réponse a été publié');
+
+            return $this->redirect($request->getUri());
+        }
+
+        return $this->render('question/show.html.twig', [
+            'question' => $question,
+            'form' => $commentForm->createView(),
+        ]);
     }
 }
