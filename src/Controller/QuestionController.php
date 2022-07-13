7438dbbfe6ffbd57a4a2e6552bfed56a18741770
Louis Carvalho <louis.carvalho98@gmail.com>
2022-07-13 10:33:20 +0200

    Creation du partial _rating

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -52,6 +52,8 @@ class QuestionController extends AbstractController
                 ->setRating(0)
                 ->setQuestion($question);
 
+            $question->setNbResponse($question->getNbResponse() + 1);
+
             $em->persist($comment);
             $em->flush();
 
