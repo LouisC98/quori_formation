4abe714b640950cf4617b231628a1c4d06c9f415
Louis Carvalho <louis.carvalho98@gmail.com>
2022-07-19 10:53:45 +0200

    Amelioration du nombre de requetes dans questionrepository

--- src/Form/QuestionType.php
+++ src/Form/QuestionType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Question;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,8 @@ class QuestionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content');
+            ->add('title', TextType::class, ['label' => 'Titre'])
+            ->add('content', TextareaType::class, ['label' => 'Contenu']);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
